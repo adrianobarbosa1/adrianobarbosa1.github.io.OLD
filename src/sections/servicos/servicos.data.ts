55abfb4d873498c44fc511acd421ca12441df9bb
adriano.barbosa100 <adriano.barbosa100@gmail.com>
2022-05-19 15:16:09 -0300

    add icons servicos

--- src/sections/servicos/servicos.data.ts
+++ src/sections/servicos/servicos.data.ts
@@ -9,37 +9,38 @@ export const servicosData = [
     {
         id: 1,
         icon: icon1,
-        title: 'Desenvolvedor de aplicativos',
+        title: 'Criação de aplicativos',
         description: `Desenvolvimento de Aplicativos para Celulares Android e iOS (iPhone).`,
     },
     {
         id: 2,
         icon: icon2,
         title: 'Criação de sites',
-        description: `Desenvolvedor de Sites, landpage, especialista em HTML, CSS, JavaScript, Node, React, MongoDB.`,
+        description: `Desenvolvimento de Sites, landpage e sistemas`,
     },
     {
         id: 3,
-        icon: icon6,
+        icon: icon3,
         title: 'Loja virtual/ Ecommerce',
-        description: `Criação de Loja Online, venda produtos ou receba pagamentos através da da Internet.`,
+        description: `Criação de Loja Online, venda de produtos ou receba pagamentos através da Internet.`,
     },
     {
         id: 4,
         icon: icon4,
         title: 'Desenvolvedor back-end',
-        description: `Especialista em Node, Express, Nest, MongoDB, Postgrees, Mysql, docker...`,
+        description: `Especialista em Node, Express, Nest, MongoDB, Postgrees, Mysql...`,
     },
     {
         id: 5,
         icon: icon5,
         title: 'Desenvolvedor front-end',
-        description: `Especialista em HTML5, JavaScript, CSS, Javascript, React.js...`,
+        description: `Especialista em HTML5, JavaScript, CSS, Next, React.js...`,
     },
     {
         id: 6,
-        icon: icon3,
-        title: 'Desenvolvedor front-end',
-        description: `Especialista em HTML5, JavaScript, CSS, Javascript, React.js...`,
+        icon: icon6,
+        title: 'Sistema integrado',
+        description: `Sistemas multiplataformas, onde dispositivos se comunicam e trabalham em sincronia.
+        `,
     },
 ]
