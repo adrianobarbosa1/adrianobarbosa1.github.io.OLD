67803b1263d2fce55eef93dcf4dd2466f7cd85fb
adrianobarbosa1 <adriano.barbosa100@gmail.com>
2022-08-02 12:57:52 -0300

    att

--- src/sections/servicos/index.tsx
+++ src/sections/servicos/index.tsx
@@ -6,23 +6,33 @@ import { servicosData } from './servicos.data';
 
 const Servicos = () => {
     return (
-        <Box as='section' id="servicos">
-            <Box pt='4.375rem' pb='7.5rem'>
-                <SectionHeading
-                    slogan='Serviços'
-                    title='Como posso te ajudar?'
-                />
+        <Flex
+            as='section'
+            id="servicos"
+            maxW='1120px'
+            h='100vh'
+            m='0 auto'
+            pt='1rem'
+            flexDir='column'
+        >
+            <Box  >
+                <Box pb='7.5rem'>
+                    <SectionHeading
+                        slogan='Serviços'
+                        title='Como posso te ajudar?'
+                    />
 
-                <Flex justify='center' >
-                    <Grid templateColumns={{ base: 'repeat(1, 6fr)', md: 'repeat(3, 2fr)' }} gap={6} maxWidth='1030px'>
-                        {servicosData?.map((item, index) => (
-                            <CardServicos key={index} data={item} />
-                        ))}
-                    </Grid >
+                    <Flex justify='center' >
+                        <Grid templateColumns={{ base: 'repeat(1, 6fr)', md: 'repeat(3, 2fr)' }} gap={6} maxWidth='1030px'>
+                            {servicosData?.map((item, index) => (
+                                <CardServicos key={index} data={item} />
+                            ))}
+                        </Grid >
 
-                </Flex>
+                    </Flex>
+                </Box>
             </Box>
-        </Box>
+        </Flex>
     );
 };
 
